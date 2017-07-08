408101a400a2b3749ff248d924b9f6d1b1133473
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2017-07-09 00:59:27 +0200

    Finalizado aprox. mitad JS

--- scripts.js
+++ scripts.js
@@ -1,7 +1,28 @@
-$(document).ready(function () {
-    $(".button-collapse").sideNav();
-    $(".dropdown-button").dropdown({
-      hover: true
-    });
-    $('select').material_select();
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $('select').material_select();
+  $(document).ready(function() {
+    $('.carousel').carousel();
+  });
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+  $('.collapsible').collapsible();
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    // stopPropagation: false // Stops event propagation
+  });
+  $('.materialboxed').materialbox();
+  $('.slider').slider();
+  $('#modal1').modal();
+  $('#modal2').modal();
+  $('#modal3').modal();
+  $('.parallax').parallax();
 });
